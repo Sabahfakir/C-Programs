68adfa1d1b2335fff540cfcfbc9fa567b87e992b
Sabahfakir <112008045+Sabahfakir@users.noreply.github.com>
2022-08-28 19:03:19 +0530

    Add files via upload

---
+++ Basic C-Program 2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+   printf("255\t Amit\t\t 09\t\t 81\t\t 150\t 50.00\n 02\t Sujata\t\t 70\t\t 80\t\t 200\t 66.67\n150\t Ajay\t\t 82\t\t 18\t\t 170\t 56.67");
+return 0;
+}
